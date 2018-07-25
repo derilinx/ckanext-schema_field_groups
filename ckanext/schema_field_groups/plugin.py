cde5d68fd8a3abdb46b7b83062a644d8c238c722
Eric Soroos <eric@derilinx.com>
2018-07-25 11:20:20 +0100

    logging

--- ckanext/schema_field_groups/plugin.py
+++ ckanext/schema_field_groups/plugin.py
@@ -1,16 +1,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import logging
+log = logging.getLogger(__name__)
 
 # groupable schema
 def list_of_groupings(schema):
+    log.debug("list_of_groupings")
     if 'dataset_field_group_sort_order' in schema:
+        log.debug("schema order: %s" % schema['dataset_field_group_sort_order'])
         return schema['dataset_field_group_sort_order']
     else:
         # unsorted
         return list(set([field['grouping'] for field in schema['dataset_fields'] if 'grouping' in field]))
-
-
 
 class SchemaFieldGroupsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
