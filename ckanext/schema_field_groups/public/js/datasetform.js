afe514a2daa645cd79cfb9de356e237cbf5eec04
Sajith Pathirathne <lakshitha@derilinx.com>
2022-09-05 08:30:06 +0000

    Ckan 2.9 and Py3 compatibility changes

---
+++ ckanext/schema_field_groups/public/js/datasetform.js
@@ -0,0 +1,20 @@
+$("#groupTab .nav-pills li:first").addClass("active");
+$("#groupTab .tab-pane:first").addClass("active fade in");
+
+// move forward
+$("#next-tab").on('click', function(e) {
+  $('.dataset-form .nav > .active').next('li').find('a').trigger('click')
+  // is there another one? if not, swap buttons
+  if ($('.dataset-form .nav > .active').next('li').length == 0) {
+    //$('#next-tab+button').show();
+    $('#next-tab').hide();
+  }
+  e.preventDefault();
+  return false;
+});
+
+// hide initially
+//$('#next-tab+button').hide();
+
+// note, uncomment the #next-tab+button selectors to hide the submit button till you've
+// hit the last tab of schema items.
