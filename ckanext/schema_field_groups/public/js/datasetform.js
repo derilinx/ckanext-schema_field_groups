675b28a0556b643090089e754bfc948a75eb10fb
Eric Soroos <eric@derilinx.com>
2025-08-28 13:31:13 +0000

    added indicators for required/errors

--- ckanext/schema_field_groups/public/js/datasetform.js
+++ ckanext/schema_field_groups/public/js/datasetform.js
@@ -13,6 +13,15 @@ $("#next-tab").on('click', function(e) {
   return false;
 });
 
+const errorTitle = ckan.i18n._("This section has a validation error");
+$('#groupTab .tab-pane:has(.error)').each((ix, elt) =>
+    $(`[aria-controls="${elt.id}"]`).addClass('error').attr('title', errorTitle));
+
+const title = ckan.i18n._("This section has a required field");
+$('#groupTab .tab-pane:has(.control-required)').each((ix, elt) =>
+    $(`[aria-controls="${elt.id}"]`).append(' <span title="' + title +'" class="control-required">*</span>'));
+
+
 // hide initially
 //$('#next-tab+button').hide();
 
